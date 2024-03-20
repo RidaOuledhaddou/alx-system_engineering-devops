70d1f3073b98f14bf5639ae45ff3a90d28fdb928
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-03-20 05:46:37 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env ruby
+# done by crypto tech coder
 
-# A regular expression to extract sender, receiver, and flags
-regex = /\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/
-
-# Extracting sender, receiver, and flags from the input using the regular expression
-matches = ARGV[0].scan(regex)
-
-# Formatting the output
-output = matches.map { |match| match.join(',') }.join("\n")
-
-# Printing the result
-puts output
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
