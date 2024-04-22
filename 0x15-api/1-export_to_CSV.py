e9fbf498e6a28d1ee21ecece41d37f304d5b32b2
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-22 04:26:13 -0700

    0x15-api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,89 +1,26 @@
 #!/usr/bin/python3
-""" module doc """
-import requests
-import sys
+"""fetches information from JSONplaceholder API and exports to CSV"""
 
-
-def main():
-    """ def com """
-    id = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/'
-    users = f'users?id={id}'
-    todos = f'todos?userId={id}'
-    done = f'{todos}&completed=true'
-    notDone = f'{todos}&completed=false'
-    userData = requests.get(f'{url}{users}').json()
-    Name = userData[0].get("name")
-    userName = userData[0].get("username")
-    todosData = requests.get(f'{url}{todos}').json()
-    todosDone = requests.get(f'{url}{done}').json()
-    doneN = len(todosDone)
-    totalN = len(todosData)
-    """Export into csv"""
-    with open(f'{id}.csv', 'w') as f:
-        for todo in todosData:
-            data = f'"{id}","{userName}","{todo.get("completed")}",'
-            data2 = f'"{todo.get("title")}"\n'
-            f.write(data+data2)
+from csv import DictWriter, QUOTE_ALL
+from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
-    main()
+    main_url = "https://jsonplaceholder.typicode.com"
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-"""
-
-export in CSV as : "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-USER_ID.csv
-
-
-
-
-Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
-     *TITLE*
-     *TITLE*
-     *TITLE*
-
-
-https://jsonplaceholder.typicode.com/users?id=1
-{
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-}
-https://jsonplaceholder.typicode.com/todos?userId=5
-[
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  ]
-
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
-  """
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"user_ID": argv[1], "username": name_result.get(
+            "username"), "completed": todo.get("completed"),
+                          "task": todo.get("title")})
+        todo_list.append(todo_dict)
+    with open("{}.csv".format(argv[1]), 'w', newline='') as f:
+        header = ["user_ID", "username", "completed", "task"]
+        writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
+        writer.writerows(todo_list)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,118 +1,33 @@
 #!/usr/bin/python3
-""" module doc """
-import json
-import requests
+"""fetches information from JSONplaceholder API and exports to JSON"""
 
-
-def main():
-    id = 1
-    data_dict = {}
-
-    while True:
-        url = "https://jsonplaceholder.typicode.com/"
-        users = f"users?id={id}"
-        todos = f"todos?userId={id}"
-        userData = requests.get(f"{url}{users}").json()
-
-        if not userData:
-            break
-
-        userName = userData[0].get("username")
-        todosData = requests.get(f"{url}{todos}").json()
-
-        data_dict[id] = [
-            {
-                "username": userName,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            }
-            for task in todosData
-        ]
-
-        id += 1
-
-    with open("todo_all_employees.json", "w") as f:
-        json.dump(data_dict, f)
-
+from json import dump
+from requests import get
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    users_result = get(users_url).json()
 
-"""Using what you did in the task #0, extend your
-Python script to export data in the JSON format.
+    big_dict = {}
+    for user in users_result:
+        todo_list = []
 
-Requirements:
+        pep_fix = "https://jsonplaceholder.typicode.com"
+        todos_url = pep_fix + "/user/{}/todos".format(user.get("id"))
+        name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+            user.get("id"))
 
-Records all tasks from all employees
-Format must be: { "USER_ID": [ {"username":
-"USERNAME", "task": "TASK_TITLE", "completed":
-TASK_COMPLETED_STATUS}, {"username": "USERNAME",
-"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ],
-"USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
- "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
-"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
-File name must be: todo_all_employees.json
+        todo_result = get(todos_url).json()
+        name_result = get(name_url).json()
+        for todo in todo_result:
+            todo_dict = {}
+            todo_dict.update({"username": name_result.get("username"),
+                              "task": todo.get("title"),
+                              "completed": todo.get("completed")})
+            todo_list.append(todo_dict)
 
-export json format
-{ "USER_ID": [{"task": "TASK_TITLE",
-"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
- {"task": "TASK_TITLE", "completed":
-TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
-File name must be: USER_ID.json
+        big_dict.update({user.get("id"): todo_list})
 
-
-export in CSV as : "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-USER_ID.csv
-
-
-
-
-Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
-     *TITLE*
-     *TITLE*
-     *TITLE*
-
-
-https://jsonplaceholder.typicode.com/users?id=1
-{
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-}
-https://jsonplaceholder.typicode.com/todos?userId=5
-[
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  ]
-
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
-  """
+    with open("todo_all_employees.json", 'w') as f:
+        dump(big_dict, f)
