3b5e61395369b54ecc4a38abcf70c5f8f622109a
RidaOuledhaddou <reda7ouledhaddou@gmail.com>
2024-05-09 20:29:07 +0000

    addtask 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" this script queries the Reddit API and returns the number of subscribers"""
+"""
+this script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-        """ queries the Reddit API and returns the number of subscribers"""
-
-        res = requests.get("https://www.reddit.com/r/{}/about.json"
-                .format(subreddit))
-        if res.status_code >= 300:
-            return 0
-
-        return res.json().get("data").get("subscribers")
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
